c0eaa4f79400aac7eab0c0a3d519b1d0a040c99c
luthfeew <luthfeew@gmail.com>
2022-10-06 22:20:22 +0700

    test beranda

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,9 @@ Route::get('/', function () {
 Route::get('/beranda', function () {
     return view('beranda');
 });
+Route::get('/berandav2', function () {
+    return view('berandav2');
+});
 Route::get('/login', function () {
     return view('login');
 });
