1e4fe9c4fa91ee1ad0947a191a673d7fdd143a21
luthfeew <luthfeew@gmail.com>
2022-10-06 20:30:00 +0700

    empty views

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/beranda', function () {
+    return view('beranda');
+});
+Route::get('/login', function () {
+    return view('login');
+});
+Route::get('/register', function () {
+    return view('register');
+});
+Route::get('/komen', function () {
+    return view('komen');
+});
+Route::get('/profil', function () {
+    return view('profil');
+});
