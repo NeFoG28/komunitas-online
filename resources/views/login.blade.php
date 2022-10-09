4e85acaa436458d224f553f6a9cc4402ea0aa522
Yusuf Indra Bayu <yusufindra.b@gmail.com>
2022-10-09 07:30:13 +0700

    asset, css, html
    login and register

---
+++ resources/views/login.blade.php
@@ -0,0 +1,25 @@
+<link rel="stylesheet" href="css/login.style.css">
+<div class="wrapper">
+        <div class="logo">
+            <img src="asset/poop.png" alt="poop">
+        </div>
+        <div class="name">
+            KO    
+        <br>
+            <p>Komunitas-Online</p>
+        </div>
+        <form class="p-3 mt-3">
+            <div class="form-field d-flex align-items-center">
+                <span class="far fa-user"></span>
+                <input type="text" name="userName" id="userName" placeholder="Username">
+            </div>
+            <div class="form-field d-flex align-items-center">
+                <span class="fas fa-key"></span>
+                <input type="password" name="password" id="pwd" placeholder="Password">
+            </div>
+            <button class="btn mt-3">Login</button>
+        </form>
+        <div class="text-center fs-6">
+            <a href="#">Forget password?</a> or <a href="/register">Sign up</a>
+        </div>
+    </div>
